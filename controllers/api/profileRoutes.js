2beddb6e0f269042bd5ca84e925a35af8c59350f
Acid0001 <pissboyree@gmail.com>
2024-07-23 21:22:48 -0700

    Stuff and Things
    Changes:
    
    Cleaned up controllers
    Added errorHandler middleware
    Added Helpers middleware to call on project data without rewriting the code in other files.
    Added helmet for securing HTTP headers
    added ratelimiting with express-rate-limit
    added route validation for project routes and userRoutes via express-validator

---
+++ controllers/api/profileRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { getProjectData } = require('../helpers');
+const withAuth = require('../../utils/auth');
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const projects = await getProjectData(req.session.user_id);
+    res.render('profile', { projects, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
