111d50d36cd4f809f35daf2f1ec61b86d358b0da
Acid0001 <pissboyree@gmail.com>
2024-07-28 15:11:00 -0700

    Added routes for all MVC
    Added MVC routes for calendar, contact, FAQ, rentspace, and instagram.

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -11,4 +11,35 @@ router.get('/', async (req, res) => {
   }
 });
 
+// About page route
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About Us', logged_in: req.session.logged_in });
+});
+
+// Calendar page route
+router.get('/calendar', (req, res) => {
+  res.render('calendar', { title: 'Calendar', logged_in: req.session.logged_in });
+});
+
+// Contact page route
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact Us', logged_in: req.session.logged_in });
+});
+
+// FAQ page route
+router.get('/faq', (req, res) => {
+  res.render('faq', { title: 'FAQ', logged_in: req.session.logged_in });
+});
+
+// Rent Space page route
+router.get('/rentspace', (req, res) => {
+  res.render('rentspace', { title: 'Rent Space', logged_in: req.session.logged_in });
+});
+
+
+// Instagram page route
+router.get('/instagram', (req, res) => {
+  res.render('instagram', { title: 'Instagram', logged_in: req.session.logged_in });
+});
+
 module.exports = router;
