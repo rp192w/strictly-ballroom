dec50fdabc9e7a44789a530291c15c9664694591
Acid0001 <pissboyree@gmail.com>
2024-07-28 22:32:02 -0700

    Homepage changes
    Made the about the default landing page

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage', { title: 'Homepage', logged_in: req.session.logged_in });
+    res.render('about', { title: 'About Us', logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
